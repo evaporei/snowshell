29911a9f040a33119d9d69b7740902b01704b684
Eva Pace <eba.pachi@gmail.com>
2023-09-03 15:43:10 -0300

    main: fork+exec sub process

--- snowshell.c
+++ snowshell.c
@@ -1,9 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
     char input[4096];
-    int res;
+    int res = 0;
+    pid_t child_pid, wpid;
 
     while (true) {
         printf("⛄️ ");
@@ -15,7 +17,11 @@ int main(void) {
             return 0;
         }
 
-        printf("%s\n", input);
+        if ((child_pid = fork()) == 0) {
+            execvp(input, NULL);
+            return 0;
+        }
+        wpid = wait(&res);
     }
     return 0;
 }
