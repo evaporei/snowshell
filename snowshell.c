ea36858a0cb306a144baea0112ab08d3d66c7701
Eva Pace <eba.pachi@gmail.com>
2023-09-03 15:05:23 -0300

    main: handle EOF (Ctrl+D)

--- snowshell.c
+++ snowshell.c
@@ -3,9 +3,18 @@
 
 int main(void) {
     char input[4096];
+    int res;
+
     while (true) {
         printf("⛄️ ");
-        scanf("%4096s", input);
+
+        res = scanf("%4096s", input);
+        if (res == EOF) {
+            printf("^D\n");
+            printf("❄❅❄❅ Goodbye and stay warm! ❄❅❄❅\n");
+            return 0;
+        }
+
         printf("%s\n", input);
     }
     return 0;
