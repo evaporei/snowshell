e830bff1491b2f6b08a80ea8c510188fff36634f
Eva Pace <eba.pachi@gmail.com>
2023-09-03 20:16:52 -0300

    main: handle multiple args in sub command

--- snowshell.c
+++ snowshell.c
@@ -1,33 +1,52 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
     char input[4096];
-    char *args[] = {input, NULL};
-    int res = 0;
+    char **args = NULL;
+    char *buf;
+    int res = 0, n_spaces = 0;
 
     while (true) {
         printf("⛄️ ");
 
-        res = scanf("%4096s", input);
-        if (res == EOF) {
+        if (fgets(input, 4096, stdin) == NULL) {
             printf("^D\n");
             printf("❄❅❄❅ Goodbye and stay warm! ❄❅❄❅\n");
             return 0;
         }
+        input[strcspn(input, "\n")] = 0;
+
+        buf = strtok(input, " ");
+        while (buf != NULL) {
+            args = realloc(args, sizeof(char *) * ++n_spaces);
+            if (args == NULL)
+                exit(-1);
+
+            args[n_spaces - 1] = buf;
+
+            buf = strtok(NULL, " ");
+        }
+
+        args = realloc(args, sizeof(char *) * n_spaces + 1);
+        args[n_spaces] = 0;
 
         if (fork() == 0) {
-            res = execvp(input, args);
+            res = execvp(args[0], args);
             // no such file or directory
             if (res == -1 && errno == 2) {
-                printf("🌲⛷  %s: command not found\n", input);
+                printf("🌲⛷  %s: command not found\n", args[0]);
             }
             return 0;
         }
         wait(&res);
+        free(args);
+        n_spaces = 0;
     }
 
     return 0;
